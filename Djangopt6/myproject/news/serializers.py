558bad745946ab41997187dd1efca3335e76e00d
Alisher <uranpowerhead@gmail.com>
2025-04-07 03:13:01 +0600

    qwe

--- Djangopt6/myproject/news/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import News
-
-class NewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = News
-        fields = ['id', 'title', 'content', 'created_at', 'author']
-        read_only_fields = ['created_at', 'author']
--- Djangoptt6/myproject/news/api_urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import news_create_api
-
-urlpatterns = [
-    path('news/', news_create_api, name='news_create_api'),
-]
